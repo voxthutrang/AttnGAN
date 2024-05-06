d996ec8bcd328518101623941426d81279be5903
Võ Thu Trang <102519408+voxthutrang@users.noreply.github.com>
2024-05-06 17:57:32 +0700

    change code/main.py

--- code/main.py
+++ code/main.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     # Get data loader
     imsize = cfg.TREE.BASE_SIZE * (2 ** (cfg.TREE.BRANCH_NUM - 1))
     image_transform = transforms.Compose([
-        transforms.Scale(int(imsize * 76 / 64)),
+        transforms.Resize(int(imsize * 76 / 64)),
         transforms.RandomCrop(imsize),
         transforms.RandomHorizontalFlip()])
     dataset = TextDataset(cfg.DATA_DIR, split_dir,
