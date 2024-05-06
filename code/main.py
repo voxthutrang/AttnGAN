2dca3befb215aa78413a48ab6c44b55c4a267d48
Võ Thu Trang <102519408+voxthutrang@users.noreply.github.com>
2024-05-06 18:01:33 +0700

    change in main.py

--- code/main.py
+++ code/main.py
@@ -39,7 +39,7 @@ def gen_example(wordtoix, algo):
     filepath = '%s/example_filenames.txt' % (cfg.DATA_DIR)
     data_dic = {}
     with open(filepath, "r") as f:
-        filenames = f.read().decode('utf8').split('\n')
+        filenames = f.read().split('\n')
         for name in filenames:
             if len(name) == 0:
                 continue
