50a0c548e95f6b3e993b8c18f9c91db612248625
Võ Thu Trang <102519408+voxthutrang@users.noreply.github.com>
2024-05-13 17:15:31 +0700

    3

--- code/datasets.py
+++ code/datasets.py
@@ -147,7 +147,7 @@ class TextDataset(data.Dataset):
         for i in range(len(filenames)):
             cap_path = '%s/text/%s.txt' % (data_dir, filenames[i])
             with open(cap_path, "r") as f:
-                captions = f.read().decode('utf8').split('\n')
+                captions = f.read().split('\n')
                 cnt = 0
                 for cap in captions:
                     if len(cap) == 0:
